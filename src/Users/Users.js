ee9d565ae48259b4632964df29f9803c1aa142d7
Simmons, T.J <tj.simmons@magellanlp.com>
2019-12-04 15:04:49 -0600

    more changes

--- src/Users/Users.js
+++ src/Users/Users.js
@@ -1,24 +1,108 @@
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
+import { getUsers, deleteUser, addUser } from "../api/userApi";
+import "./users.css";
 
 const Users = function() {
   // array destructuring
   const [users, setUsers] = useState([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [user, setUser] = useState({
+    id: null,
+    name: "",
+    role: ""
+  });
+  const [isSaving, setIsSaving] = useState(false);
 
-  const deleteUser = function(id) {
-    setUsers(users.filter(u => u.id !== id));
+  useEffect(() => {
+    getUsers()
+      .then(users => setUsers(users))
+      .finally(() => setIsLoading(false));
+  }, []); // second argument is a array of dependencies - empty array means only run once, not every time this is re-rendered
+
+  const handleDeleteUser = function(id) {
+    deleteUser(id).then(() => setUsers(users.filter(u => u.id !== id)));
   };
+
+  const handleSubmit = function(e) {
+    e.preventDefault();
+
+    setIsSaving(true);
+
+    addUser(user)
+      .then(savedUser => setUsers([...users, savedUser]))
+      .finally(() => {
+        setUser({ name: "", role: "" });
+
+        setIsSaving(false);
+      });
+  };
+
+  const handleChange = function({ target }) {
+    setUser({ ...user, [target.name]: target.value });
+  };
+
+  if (isLoading) {
+    return "Loading...";
+  }
 
   return (
     <>
       <h1>Users</h1>
-      <ul>
-        {users.length === 0 && "No users."}
-        {users.map(u => (
-          <li key={u.id}>
-            <button onClick={() => deleteUser(u.id)}>Delete</button> {u.name}
-          </li>
-        ))}
-      </ul>
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label htmlFor="name">Name</label>
+          <br />
+          <input
+            type="text"
+            id="name"
+            name="name"
+            value={user.name}
+            onChange={handleChange}
+          />
+        </div>
+        <div>
+          <label htmlFor="role">Role</label>
+          <br />
+          <input
+            type="text"
+            id="role"
+            name="role"
+            value={user.role}
+            onChange={handleChange}
+          />
+        </div>
+        <input
+          type="submit"
+          value="Save"
+          disabled={isSaving ? "disabled" : ""}
+        />
+      </form>
+      <br />
+      <table>
+        <thead>
+          <tr>
+            <th></th>
+            <th>Name</th>
+            <th>Role</th>
+          </tr>
+        </thead>
+        <tbody>
+          {users.length === 0 && (
+            <tr>
+              <td colSpan="3">No users.</td>
+            </tr>
+          )}
+          {users.map(u => (
+            <tr key={u.id}>
+              <td>
+                <button onClick={() => handleDeleteUser(u.id)}>Delete</button>
+              </td>
+              <td>{u.name}</td>
+              <td>{u.role}</td>
+            </tr>
+          ))}
+        </tbody>
+      </table>
     </>
   );
 };
---
+++ src/api/userApi.js
@@ -0,0 +1,37 @@
+const baseUrl = process.env.REACT_APP_USER_API_URL + "/users";
+
+export const getUsers = function() {
+  return fetch(baseUrl).then(response => {
+    if (response.ok) {
+      return response.json();
+    } else {
+      throw new Error("Bad network response");
+    }
+  });
+};
+
+export const deleteUser = function(id) {
+  return fetch(`${baseUrl}/${id}`, { method: "DELETE" }).then(response => {
+    if (response.ok) {
+      return response.json();
+    } else {
+      throw new Error("Bad network response");
+    }
+  });
+};
+
+export const addUser = function(user) {
+  return fetch(baseUrl, {
+    method: "POST",
+    body: JSON.stringify(user),
+    headers: {
+      "content-type": "application/json"
+    }
+  }).then(response => {
+    if (response.ok) {
+      return response.json();
+    } else {
+      throw new Error("Bad network response");
+    }
+  });
+};
